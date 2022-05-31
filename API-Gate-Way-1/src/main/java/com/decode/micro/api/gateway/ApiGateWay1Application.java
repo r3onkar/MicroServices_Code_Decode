559fb74ff29aabd0a41cd667a3bd3b5d1adf8c6a
r3onkar <97029116+r3onkar@users.noreply.github.com>
2022-05-31 15:58:11 +0530

    First Upload
    MicroServices WIth Citizen Services , And Vaccination Center , with API GATE WAY, and EUREKA SERVER.

---
+++ API-Gate-Way-1/src/main/java/com/decode/micro/api/gateway/ApiGateWay1Application.java
@@ -0,0 +1,13 @@
+package com.decode.micro.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateWay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateWay1Application.class, args);
+	}
+
+}
---
+++ API-Gate-Way-1/src/test/java/com/decode/micro/api/gateway/ApiGateWay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decode.micro.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWay1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
